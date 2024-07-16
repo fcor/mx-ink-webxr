c1877e1d2e7920be784b0bb434e45e04fc5c3799
Fabio Cortes <fcor14@hotmail.com>
2024-07-16 20:51:17 +0200

    Base files

---
+++ src/script.js
@@ -0,0 +1,59 @@
+import * as THREE from "three";
+import { TubePainter } from "three/addons/misc/TubePainter.js";
+import { XRButton } from "three/addons/webxr/XRButton.js";
+
+let camera, scene, renderer;
+let controller1, controller2;
+
+const cursor = new THREE.Vector3();
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+init();
+
+function init() {
+  const canvas = document.querySelector("canvas.webgl");
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 50);
+  camera.position.set(0, 1.6, 3);
+
+  const grid = new THREE.GridHelper(4, 1, 0x111111, 0x111111);
+  scene.add(grid);
+
+  scene.add(new THREE.HemisphereLight(0x888877, 0x777788, 3));
+
+  const light = new THREE.DirectionalLight(0xffffff, 1.5);
+  light.position.set(0, 4, 0);
+  scene.add(light);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer.setPixelRatio(window.devicePixelRatio, 2);
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setAnimationLoop(animate);
+  renderer.xr.enabled = true;
+}
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+
+function animate() {
+
+  // Render
+  renderer.render(scene, camera);
+};
