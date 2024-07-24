03a05235140b040283750f952fb242d8de6157ab
Fabio Cortes <fcor14@hotmail.com>
2024-07-24 18:44:19 +0200

    Added HTTPS and first drawing test

--- src/script.js
+++ src/script.js
@@ -33,11 +33,37 @@ function init() {
   const painter1 = new TubePainter();
   scene.add(painter1.mesh);
 
+  const painter2 = new TubePainter();
+  scene.add(painter2.mesh);
+
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
   renderer.setPixelRatio(window.devicePixelRatio, 2);
   renderer.setSize(sizes.width, sizes.height);
   renderer.setAnimationLoop(animate);
   renderer.xr.enabled = true;
+  document.body.appendChild(XRButton.createButton(renderer));
+
+  controller1 = renderer.xr.getController(0);
+  controller1.addEventListener("selectstart", onSelectStart);
+  controller1.addEventListener("selectend", onSelectEnd);
+  controller1.userData.painter = painter1;
+  scene.add(controller1);
+
+  controller2 = renderer.xr.getController(1);
+  controller2.addEventListener("selectstart", onSelectStart);
+  controller2.addEventListener("selectend", onSelectEnd);
+  controller2.userData.painter = painter2;
+  scene.add(controller2);
+
+  const pivot = new THREE.Mesh(new THREE.IcosahedronGeometry(0.01, 3));
+  pivot.name = "pivot";
+  pivot.position.z = -0.03;
+
+  const group = new THREE.Group();
+  group.add(pivot);
+
+  controller1.add(group.clone());
+  controller2.add(group.clone());
 }
 
 window.addEventListener("resize", () => {
@@ -55,6 +81,48 @@ window.addEventListener("resize", () => {
 });
 
 function animate() {
+  handleController(controller1);
+  handleController(controller2);
   // Render
   renderer.render(scene, camera);
+}
+
+function handleController(controller) {
+  controller.updateMatrixWorld(true);
+
+  const userData = controller.userData;
+  const painter = userData.painter;
+
+  const pivot = controller.getObjectByName("pivot");
+
+  if (userData.isSqueezing === true) {
+    const delta = (controller.position.y - userData.positionAtSqueezeStart) * 5;
+    const scale = Math.max(0.1, userData.scaleAtSqueezeStart + delta);
+
+    pivot.scale.setScalar(scale);
+    painter.setSize(scale);
+  }
+
+  cursor.setFromMatrixPosition(pivot.matrixWorld);
+
+  if (userData.isSelecting === true) {
+    painter.lineTo(cursor);
+    painter.update();
+  }
+}
+
+function onSelectStart() {
+  this.updateMatrixWorld(true);
+
+  const pivot = this.getObjectByName("pivot");
+  cursor.setFromMatrixPosition(pivot.matrixWorld);
+
+  const painter = this.userData.painter;
+  painter.moveTo(cursor);
+
+  this.userData.isSelecting = true;
+}
+
+function onSelectEnd() {
+  this.userData.isSelecting = false;
 }
